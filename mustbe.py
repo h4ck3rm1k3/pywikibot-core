7fe950f607bf16745e03331638aa0a9f95c6e943
James Michael DuPont <jamesmikedupont@gmail.com>
2013-12-04 07:05:33 -0600

    more cleaning and putzing
    Change-Id: I272fdf96678fb45002d798670d47a110c29a6e2b

---
+++ mustbe.py
@@ -0,0 +1,28 @@
+def must_be(group=None, right=None):
+    """ Decorator to require a certain user status. For now, only the values
+        group = 'user' and group = 'sysop' are supported. The right property
+        will be ignored for now.
+
+        @param group: the group the logged in user should belong to
+                      legal values: 'user' and 'sysop'
+        @param right: the rights the logged in user hsould have
+                      not supported yet and thus ignored.
+        @returns: a decorator to make sure the requirement is statisfied when
+                  the decorated function is called.
+    """
+    if group == 'user':
+        run = lambda self: self.login(False)
+    elif group == 'sysop':
+        run = lambda self: self.login(True)
+    else:
+        raise Exception("Not implemented")
+
+    def decorator(fn):
+        def callee(self, *args, **kwargs):
+            run(self)
+            return fn(self, *args, **kwargs)
+        callee.__name__ = fn.__name__
+        callee.__doc__ = fn.__doc__
+        return callee
+
+    return decorator
---
+++ pywikibot/site/loginstatus.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8  -*-
+"""
+Objects representing MediaWiki sites (wikis) and families (groups of wikis
+on the same topic in different languages).
+from pywikibot.site import BaseSite
+"""
+#
+# (C) Pywikibot team, 2008-2012
+#
+# Distributed under the terms of the MIT license.
+#
+__version__ = '$Id$'
+
+#try:
+#    from hashlib import md5
+#except ImportError:
+#    #from md5 import md5
+#import httplib2
+#import datetime
+#import itertools
+#import os
+#import re
+#import sys
+#import threading
+#import time
+#import urllib.request, urllib.parse, urllib.error
+#import json
+
+#import pywikibot
+#from pywikibot import deprecate_arg
+#from pywikibot import config
+#from pywikibot import deprecated
+#from pywikibot.bot import log
+#from pywikibot import pagegenerators
+#from pywikibot.throttle import Throttle
+#from pywikibot.data import api
+#import pywikibot.data.api as api
+#from pywikibot.exceptions import (
+    #NoSuchSite, 
+    #Error, 
+    #UserBlocked,
+    #NoPage,
+    #NoUsername,
+    #EditConflict, 
+    #SpamfilterError, 
+    #LockedPage
+#)
+
+#from pywikibot.deprecate import deprecated
+#from pywikibot.deprecate import deprecate_arg
+
+
+_logger = "wiki.site"
+
+class LoginStatus(object):
+    """ Enum for Login statuses.
+
+    >>> LoginStatus.NOT_ATTEMPTED
+    -3
+    >>> LoginStatus.AS_USER
+    0
+    >>> LoginStatus.name(-3)
+    'NOT_ATTEMPTED'
+    >>> LoginStatus.name(0)
+    'AS_USER'
+    """
+    NOT_ATTEMPTED = -3
+    IN_PROGRESS = -2
+    NOT_LOGGED_IN = -1
+    AS_USER = 0
+    AS_SYSOP = 1
+
+    @classmethod
+    def name(cls, search_value):
+        for key, value in list(cls.__dict__.items()):
+            if key == key.upper() and value == search_value:
+                return key
+        raise KeyError("Value %r could not be found in this enum"
+                       % search_value)
+
+
+
