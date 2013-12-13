d707c67d6f56f270ed47153e9449f5d7503ee42f
James Michael DuPont <jamesmikedupont@gmail.com>
2013-12-12 19:09:35 -0600

    refactoring
    Change-Id: Ib735b3d0724d327bdefff84717a9af86aeb66477

---
+++ processing/nltk/__init__.py
@@ -0,0 +1,74 @@
+import nltk
+from pywikibot.families.wikipedia_family import Family as WikipediaFamily
+from pywikibot.page import Page
+#from pywikibot.site.base import BaseSite as Site
+from pywikibot.site.apisite import APISite as Site
+from pywikibot.bot import debug
+import os 
+
+def page(name):
+    enwiki = Site("en", WikipediaFamily())
+    p = Page(enwiki, name)
+
+#    data = enwiki.loadrevisions(p, getText=True)
+
+    data = p.get()
+    #print("Data:%s" % data)
+    return data
+    #link = Link(u"Kosovo", self.enwiki)
+
+def extract_entity_names(t):
+    entity_names = []
+    #    if hasattr(t, 'label') and t.label():
+    #        print ("Label:%s" % t.label())
+    
+    for child in t:
+
+        if len(child) == 1:
+            entity_names.append(
+                child[0][0]
+            )
+            #print ("child:%s" % str(child))
+
+        #for child2 in child:
+        #print ("child2:%s" % str(child2))
+          
+
+#        if t.node == 'NE':
+
+#        else:
+#            for child in t:
+#                entity_names.extend(extract_entity_names(child))
+    return entity_names
+
+
+
+def parse_page(name):
+
+    filename="data/%s.wiki" % name
+    if not os.path.exists(filename):
+        sample = page(name)
+        of  = open(filename, 'wb')
+        of.write(sample.encode("utf-8"))
+    else:
+        of  = open(filename, 'rb')
+        sample = of.read().decode("utf-8")
+    of.close
+
+    sentences = nltk.sent_tokenize(sample)
+    tokenized_sentences = [nltk.word_tokenize(sentence) for sentence in sentences]
+    tagged_sentences = [nltk.pos_tag(sentence) for sentence in tokenized_sentences]
+    chunked_sentences = nltk.batch_ne_chunk(tagged_sentences, binary=True)
+    
+    entity_names = []
+    for tree in chunked_sentences:
+        # Print results per sentence
+        # print extract_entity_names(tree)
+        entity_names.extend(extract_entity_names(tree))
+ 
+    # Print all entity names
+#    print (entity_names)
+ 
+    # Print unique entity names
+    debug ("RESULT %s " % str(set(entity_names)).encode("utf-8"))
+    return entity_names
---
+++ processing/nltk/tests/lawrence.py
@@ -0,0 +1,9 @@
+u'''
+from https://gist.github.com/322906/90dea659c04570757cccf0ce1e6d26c9d06f9283
+'''
+from processing.nltk import parse_page
+
+parse_page("Lawrence, Kansas")
+
+    # example output
+entities = {}
